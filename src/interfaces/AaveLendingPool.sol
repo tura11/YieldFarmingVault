9df1e4571e68e951e81a4347c2568f8a5e83c652
Tymek <tymek_uramek@wp.pl>
2025-11-09 22:11:12 +0100

    configs

--- src/interfaces/AaveLendingPool.sol
+++ src/interfaces/AaveLendingPool.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+
 interface IAaveLendingPool {
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
     function withdraw(address asset, uint256 amount, address to) external returns (uint256);
